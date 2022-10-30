a81de70853baab9a2168634111f98f2bf2538b3a
brepowell <85724630+brepowell@users.noreply.github.com>
2022-10-30 15:27:42 -0700

    Breanna -- added more strings to strings.xml

--- MeetMe/app/src/main/java/com/css545/meetme/ui/ConsentScreen.kt
+++ MeetMe/app/src/main/java/com/css545/meetme/ui/ConsentScreen.kt
@@ -2,8 +2,15 @@ package com.css545.meetme.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.material.Text
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ConsentScreen() {
     Text("Consent Screen")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewConsentScreen() {
+    ConsentScreen()
 }
