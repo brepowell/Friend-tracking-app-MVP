e069069668d9413f8dbacf1f8c47ababd3340c7b
brepowell <85724630+brepowell@users.noreply.github.com>
2022-11-06 16:17:00 -0800

    Breanna - refactored and renamed our files to alphabetize them

--- MeetMe/app/src/main/java/com/css545/meetme/ui/ScreenMap.kt
+++ MeetMe/app/src/main/java/com/css545/meetme/ui/ScreenMap.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 
 import androidx.compose.runtime.Composable
-import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
