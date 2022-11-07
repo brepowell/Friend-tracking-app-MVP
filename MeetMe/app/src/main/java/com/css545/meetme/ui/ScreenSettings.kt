9e4609ed71d86e83f70fec1bb99a395e1c2f60c2
brepowell <85724630+brepowell@users.noreply.github.com>
2022-11-06 16:17:00 -0800

    Breanna - refactored and renamed our files to alphabetize them

--- MeetMe/app/src/main/java/com/css545/meetme/ui/ScreenSettings.kt
+++ MeetMe/app/src/main/java/com/css545/meetme/ui/ScreenSettings.kt
@@ -1,16 +1,9 @@
 package com.css545.meetme.ui
 
-import android.content.Context
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CutCornerShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.*
@@ -18,15 +11,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.css545.meetme.R
 import com.css545.meetme.data.SettingsState
 import com.css545.meetme.ui.components.CustomButton
 import com.css545.meetme.ui.components.CustomTextField
