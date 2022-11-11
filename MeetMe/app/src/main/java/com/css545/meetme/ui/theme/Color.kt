37c0941a5307b2daa2a524db9bd41dc5d60c5d3d
brepowell <85724630+brepowell@users.noreply.github.com>
2022-11-11 15:41:57 -0800

    Breanna - working on permissions setup

--- MeetMe/app/src/main/java/com/css545/meetme/ui/theme/Color.kt
+++ MeetMe/app/src/main/java/com/css545/meetme/ui/theme/Color.kt
@@ -6,12 +6,10 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+/** OUR MEETME APP COLORS */
 val colorRichBlackFogra39 = Color(0xff090809)
 val colorTiffanyBlue = Color(0xff17bebb)
-
-/*
 val colorVividTangerine = Color(0xffffa69e)
-
 val colorJonquil = Color(0xfff9c80e)
 val colorMeetMePurple = Color(0xff744fc6)
-*/
